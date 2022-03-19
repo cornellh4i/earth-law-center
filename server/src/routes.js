ecc1cac50773dffa38f112bbf304968ff43ca970
Yao Yichen <yy486@cornell.edu>
2022-03-19 16:28:38 -0400

    change .env redirect_uris

--- server/src/routes.js
+++ server/src/routes.js
@@ -29,12 +29,12 @@ module.exports = () => {
     // res.json({ msg: `My favorite food is, ${req.params.foodfav}` });
   });
 
-  index.authorizeInsertText(functions.insertText, "1w3YFbfJ4y5Fz7ea0_5YTgxE9zoA3qvOnlKoRFmKw3Os", "This text is up!", { index: 1 });
+  // index.authorizeInsertText(functions.insertText, "1w3YFbfJ4y5Fz7ea0_5YTgxE9zoA3qvOnlKoRFmKw3Os", "This text is up!", { index: 1 });
   // Endpoint for insertText()
   router.get('/insertText/:docID/:text/:location', async (req, res) => {
-    // res.json({ msg: `docid: ${req.params.docID}, text: ${req.params.text}, location: ${req.params.location}` });
-    // console.log(JSON.parse(req.params.location));
-    // authorizeInsertText(insertText, "1w3YFbfJ4y5Fz7ea0_5YTgxE9zoA3qvOnlKoRFmKw3Os", "This text is up!", { index: 1 });
+    index.authorizeInsertText(functions.insertText, req.params.docID, req.params.text, { index: req.params.location});
+    res.json({ msg: `docid: ${req.params.docID}, text: ${req.params.text}, location: ${req.params.location}` });
+    console.log(JSON.parse(req.params.location));
   });
 
   // Endpoint for replaceAllText() -- TODO
