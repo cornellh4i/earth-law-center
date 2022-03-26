c31577a05ddc387fe332eb4a732c5ea702714595
aoa9 <aoa9@cornell.edu>
2022-03-26 19:15:08 -0400

    Different images for each card

--- client/src/components/LandingCard/LandingCard.jsx
+++ client/src/components/LandingCard/LandingCard.jsx
@@ -9,13 +9,14 @@ import { CardActionArea } from '@mui/material';
 
 const LandingCard = (props) => {
     return ( 
+        
         <Card sx={{maxWidth: 345, m:5}}>
             <CardActionArea sx = {{bgcolor: 'text.disabled'}}>
             <CardMedia
             component="img"
             alt="template img"
             height="200"
-            src = "https://images.unsplash.com/photo-1617203443952-6d2619f7ff4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"/>
+            src = {props.imgsrc}/>
             <CardContent>
                 <Typography gutterBottom variant="h5" component="div" sx = {{fontWeight: 'bold'}}>
                 {props.title}
--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -12,8 +12,14 @@ const Landing = () => {
     <div>
       {/*We have two Landing boxes for now*/}
       <div className='box'>
-        <LandingCard title = {"Law Template"} description= {"One sentence description of what is on this webpage"}></LandingCard>
-        <LandingCard title = {"Letter Template"} description = {"One sentence description of what is on this webpage"}></LandingCard>
+        <LandingCard 
+        title = {"Law Template"} 
+        description= {"One sentence description of what is on this webpage"}
+        imgsrc = {"https://images.unsplash.com/photo-1617203443952-6d2619f7ff4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"}></LandingCard>
+        <LandingCard 
+        title = {"Letter Template"} 
+        description = {"One sentence description of what is on this webpage"}
+        imgsrc = {"https://media.istockphoto.com/photos/definition-legislation-picture-id182171560?s=612x612"}></LandingCard>
       </div>
 
       {/*Three resource boxes I believe. We can probably useState to fix them later*/}
