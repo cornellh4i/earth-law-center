382b328a5ee7be0854d9dde1a8bf07efa40a3f3b
Jason Zheng <jasonz4200@gmail.com>
2022-03-30 22:00:17 -0400

    Revert "Delete redundant TemplateCard component"
    This reverts commit a9d7adf79354c74f14bde6c571f2f9e7b4d412ad.

---
+++ client/src/components/TemplateCard/TemplateCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './TemplateCards.css';
+
+/** Component for TemplateCard Page */
+
+const TemplateCard = () => {
+  return (
+    <div>
+      <h1>TemplateCard</h1>
+    </div>
+  );
+};
+export default TemplateCard;
