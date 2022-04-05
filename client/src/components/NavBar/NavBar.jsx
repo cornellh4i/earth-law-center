9ccd20c6ba29a6db0fa7c65317b43b30a053da9c
Jason Zheng <jasonz4200@gmail.com>
2022-04-04 20:57:13 -0400

    Implement page navigation

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './NavBar.css';
 import Button from '../Button/Button'; 
 import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 // TODOS: 
@@ -11,17 +12,21 @@ import { useNavigate } from 'react-router-dom';
 
 /** A Navigation Bar for the Website */ 
 const NavBar = () => {
-  const navigate = useNavigate();
-  function handleClick() {
-    // TODO: Implement a function that when passed in a page name, will navigate to that page
-    // Tip --> look into react router dom!
-    navigate('/laws');
-    console.log("handleClick not yet implemented") /* Delete this when finished implementing nav bar */
-  }
+  // const navigate = useNavigate();
+  // function handleClick() {
+  //   // TODO: Implement a function that when passed in a page name, will navigate to that page
+  //   // Tip --> look into react router dom!
+  //   navigate('/laws');
+  //   console.log("handleClick not yet implemented") /* Delete this when finished implementing nav bar */
+  // }
   return (
     <div className = "navbar-container">
-      <h1>NavBar Here</h1> {/* Delete this when finished implementing nav bar */}
-      <Button text = "Laws" css = "navbar-btn" handleClick={handleClick}/>
+      <Link to="/laws"><Button text = "Laws" css = "navbar-btn" /></Link>
+      <Link to="/letters"><Button text = "Letters" css = "navbar-btn" /></Link>
+      <Link to="/landing"><Button text = "About" css = "navbar-btn" /></Link>
+      {/* <Button text = "Laws" css = "navbar-btn" handleClick={handleClick}/>
+      <Button text = "Letters" css = "navbar-btn" handleClick={handleClick}/>
+      <Button text = "About" css = "navbar-btn" handleClick={handleClick}/> */}
     </div>
   );
 };
