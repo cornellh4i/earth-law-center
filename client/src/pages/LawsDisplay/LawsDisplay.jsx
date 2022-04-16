58f548a19fa492ca9ab478be330c5c47e0beb0bd
Suhani Patel <sjp269@cornell.edu>
2022-04-16 17:23:28 -0400

    outline for filter

--- client/src/pages/LawsDisplay/LawsDisplay.jsx
+++ client/src/pages/LawsDisplay/LawsDisplay.jsx
@@ -3,6 +3,7 @@ import './LawsDisplay.css';
 import '../../components/CategorySideBar/CategorySideBar'; 
 import CategorySideBar from '../../components/CategorySideBar/CategorySideBar';
 import TemplateCard from '../../components/TemplateCard/TemplateCard'; 
+import MultiSelectFilter from '../../components/MultiSelectFilter/MultiSelectFilter'
 
 /** Component for Laws Page */
 const LawsDisplay = () => {
@@ -11,6 +12,7 @@ const LawsDisplay = () => {
       <h1>LawsDisplay</h1>
       <CategorySideBar/>
       <div className="testBox">
+        <MultiSelectFilter></MultiSelectFilter>
         <TemplateCard title="title 3" summary="test summary ahhahahahahahahahah"/>
         <TemplateCard title="title 2" summary="test summary numbah 2 ahhahahahahahahahah"/>
         <TemplateCard title="title 3" summary="test summary numbah 3 oh baby"/>
