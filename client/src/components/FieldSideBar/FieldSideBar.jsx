0f0f003ee9f36d744b1bf5fd84e8ceb4f045d2e1
Jason Zheng <jasonz4200@gmail.com>
2022-04-20 20:37:03 -0400

    Refactor code and general style cleanup

--- client/src/components/FieldSideBar/FieldSideBar.jsx
+++ client/src/components/FieldSideBar/FieldSideBar.jsx
@@ -1,40 +1,37 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './FieldSideBar.css';
 
-
-
-/** Component for FieldSideBar */
+/** Component for a Field Side Bar
+ * @param {field} is a list of strings containing all fields in a Google Doc
+*/
 
 const FieldSideBar = (props) => {
-
-  const [click, setClick] = useState(''); 
-
+  const [click, setClick] = useState('');
   const fieldItem = props.field.map((field) =>
-      <li>
-        <button className='side-btn' onClick={()=> setClick(field)}>{field}</button>
-      </li>
-
+    <li>
+      <button className='side-btn' onClick={() => setClick(field)}>{field}</button>
+    </li>
   )
 
-// To test if state works
+  // To test if state works
   const TestState = () => {
     // Clicked button should show up on the screen 
     return <h1>{click}</h1>
   }
 
   return (
-      <div className='sidebar-component'>
-        {/* Space for progress bar */}
-        <div>
-          <h1>Progress Bar</h1>
-        </div>
-        {/* *** */}
-        <ul className='bar-ul'>{fieldItem}</ul>
-        
-        {/* Testing clicked saved correctly */}
-        <TestState/>
-      </div> 
+    <div className='sidebar-component'>
+      {/* Space for progress bar */}
+      <div>
+        <h1>Progress Bar</h1>
+      </div>
+      {/* *** */}
+      <ul className='bar-ul'>{fieldItem}</ul>
 
+      {/* Testing clicked saved correctly */}
+      <TestState />
+    </div>
   );
 };
+
 export default FieldSideBar;
--- client/src/pages/TemplateFiller/TemplateFiller.jsx
+++ client/src/pages/TemplateFiller/TemplateFiller.jsx
@@ -3,14 +3,17 @@ import './TemplateFiller.css';
 import FieldSideBar from '../../components/FieldSideBar/FieldSideBar';
 
 /** Component for Template Filler Page */
-
 const TemplateFiller = () => {
   return (
     <div>
       {/* Entries to field prop is temporary for testing */}
-      <FieldSideBar field = {['Name of Local Ecosystem(s)', 'Municipality', 'City & State', 
-      'Activites Your Community Engages in', 'Specific Indegenous Peoples', 'Types of Impact'
-      ]}/>
+      <FieldSideBar field={['Name of Local Ecosystem(s)',
+        'Municipality',
+        'City & State',
+        'Activites Your Community Engages in',
+        'Specific Indegenous Peoples',
+        'Types of Impact'
+      ]} />
     </div>
   );
 };
