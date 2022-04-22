294ab733e38c057e701bfc31463ab296b0db664a
Jason Zheng <jasonz4200@gmail.com>
2022-04-22 13:59:04 -0400

    Implement skeleton code and props documentation

---
+++ client/src/components/QuestionAnswer/QuestionAnswer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './QuestionAnswer.css';
+
+/** Component for a Q&A component on the Template Form Filler page
+ * @param {field} is the field name selected in the navigation sidebar
+ * @param {title} is the title of the template law/letter
+ * @param {questions} is a list of lists; each nested list is a single question
+ * of the following format: ["<input type>", "<question content"]. Valid input
+ * types currently include "text input", "dropdown select", "states dropdown select"
+*/
+const QuestionAnswer = () => {
+  return (
+      <div></div>
+  );
+};
+
+export default QuestionAnswer;
