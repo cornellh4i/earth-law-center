ee1d9af8a4056ab4fb7d87b7bb520ddaad1414fc
YichenYao <yy486@cornell.edu>
2022-04-21 22:38:15 -0400

    finish most styling of resoursecard

--- client/src/components/ResourceCard/ResourceCard.jsx
+++ client/src/components/ResourceCard/ResourceCard.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
 import './ResourceCard.css';
+import Button from '../Button/Button';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import CardMedia from '@mui/material/CardMedia';
-import { CardActionArea } from '@mui/material';
+// import { CardActionArea, CardActions } from '@mui/material';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-// import Item from  '@mui/material/Item';
+
+const styles = {
+  card: {
+    maxWidth: '40%',
+    maxHeight: '100%',
+    border: 'solid black 0.05rem',
+    backgroundColor: '#fff',
+    borderRadius: '0.5rem',
+    margin: '2%',
+    padding: '0% 0% 0% 0.5%'
+  },
+  button: {
+    display: 'flex',
+    justifyContent: 'right',
+    // backgroundColor: '#B4E197',
+    width: '100%',
+    margin: '5% 0% 0%'
+  },
+  text: {
+    fontSize: 13,
+    color: "#000"
+  }
+};
+
 
 /** Component for a landing card used by template law/letter cards
  *  @param {title} is the title on the card
@@ -17,36 +41,70 @@ import Grid from '@mui/material/Grid';
 const ResourceCard = (props) => {
   const { title, resource_type, link_to_resource, description } = props;
   return (
-    <Card sx={{ maxWidth: "40%" }}>
-      <CardActionArea>
+    <Card sx={styles.card} elevation={0}>
+      <CardContent>
+        <Box component={Stack} direction='column' justifyContent='center'>
+          <Typography gutterBottom variant='h5' component='div' sx={{ fontWeight: 'bold' }}>
+            {title}
+          </Typography>
 
+          <Grid container spacing={1}>
+            {resource_type === 'Doc' &&
+              <>
+                <Grid item xs={5}>
+                  <CardMedia component='img' alt='first' height='15%' image={link_to_resource} />
+                </Grid>
 
+                <Grid item xs={7}>
+                  <Typography variant='body2' color='text.secondary' sx={styles.text} >
+                    {description}
+                  </Typography>
+                  <div style={styles.button}>
+                    <Button
+                      text='VIEW'
+                      css='grey-median-btn' />
+                  </div>
 
-        <CardContent className='card-content'>
-          <Box component={Stack} direction="column" justifyContent="center">
-            <Typography gutterBottom variant='h5' component='div' sx={{ fontWeight: 'bold' }}>
-              {title}
-            </Typography>
-
-            <Grid container spacing={2}>
-              {resource_type === "Video" &&
+                </Grid>
+              </>
+            }
+            {resource_type === 'Video' &&
+              <>
                 <Grid item xs={6}>
-                  <CardMedia component='img' alt="first" height='15%' image={link_to_resource} />
-                </Grid>}
-              {resource_type === "Doc" &&
+                  <CardMedia component='img' alt='first' height='20%' image={link_to_resource} />
+                </Grid>
                 <Grid item xs={6}>
-                  This is doc
-                  <CardMedia component='img' alt="first" height='20%' image={link_to_resource} />
-                </Grid>}
-              <Grid item xs={6}>
-                <Typography variant='body2' color='text.secondary' sx={{ fontSize: 18 }} >
-                  {description}
-                </Typography>
-              </Grid>
-            </Grid>
-          </Box>
-        </CardContent>
-      </CardActionArea>
+                  <Typography variant='body2' color='text.secondary' sx={styles.text} >
+                    {description}
+                  </Typography>
+                </Grid>
+                <div style={styles.button}>
+                  <Button
+                    text='VIEW'
+                    css='grey-median-btn' />
+                </div>
+              </>
+
+            }
+
+            {resource_type === 'Website' &&
+              <>
+                <Grid item xs={12}>
+                  <Typography variant='body2' color='text.secondary' sx={styles.text} >
+                    {description}
+                  </Typography>
+                </Grid>
+
+                <div style={styles.button}>
+                  <Button
+                    text='VIEW'
+                    css='grey-median-btn' />
+                </div>
+              </>
+            }
+          </Grid>
+        </Box>
+      </CardContent>
     </Card >
   );
 };
--- client/src/pages/ResourceDisplay/ResourceDisplay.jsx
+++ client/src/pages/ResourceDisplay/ResourceDisplay.jsx
@@ -13,28 +13,28 @@ const ResourceDisplay = () => {
       <Typography pt={2} pb={2} align='left' variant='h5' component='div' sx={{ fontWeight: 'bold' }}>Videos</Typography>
       <Grid container spacing={3}>
         <Grid item md={6} xs={12}>
-          <ResourceCard title="Video Title1" resource_type="Video" link_to_resource="../../properties/elc-home-mar2022.jpeg" description="lengendary loreum texts"></ResourceCard>
+          <ResourceCard title="Video Title1" resource_type="Video" link_to_resource="../../properties/elc-home-mar2022.jpeg" description="Lorem ipsum dolor sit amet, in sed percipitur eloquentia Lorem ipsum dolor sit amet, in sed"></ResourceCard>
         </Grid>
         <Grid item md={6} xs={12}>
-          <ResourceCard title="Video Title2" resource_type="Video" link_to_resource="../../properties/elc-home-mar2022.jpeg" description="lengendary loreum texts"></ResourceCard>
-        </Grid>
-      </Grid>
-
-      <Grid container spacing={3}>
-        <Grid item md={6} xs={12}>
-          <ResourceCard title="Doc Title1" resource_type="Doc" link_to_resource="../../properties/elc-home-mar2022.jpeg" description="lengendary loreum texts"></ResourceCard>
-        </Grid>
-        <Grid item md={6} xs={12}>
-          <ResourceCard title="Doc Title2" resource_type="Doc" link_to_resource="../../properties/elc-home-mar2022.jpeg" description="lengendary loreum texts"></ResourceCard>
+          <ResourceCard title="Video Title2" resource_type="Video" link_to_resource="../../properties/elc-home-mar2022.jpeg" description="Lorem ipsum dolor sit amet, in sed percipitur eloquentia Lorem ipsum dolor sit amet, in sed"></ResourceCard>
         </Grid>
       </Grid>
 
       <Grid container spacing={3}>
         <Grid item md={6} xs={12}>
-          <ResourceCard title="Website Title1" resource_type="Website" link_to_resource="../../properties/elc-home-mar2022.jpeg" description="lengendary loreum texts"></ResourceCard>
+          <ResourceCard title="Doc Title1" resource_type="Doc" link_to_resource="../../properties/elc-home-mar2022.jpeg" description="Lorem ipsum dolor sit amet, in sed percipitur eloquentia Lorem ipsum dolor sit amet, in sed"></ResourceCard>
         </Grid>
         <Grid item md={6} xs={12}>
-          <ResourceCard title="Website Title2" resource_type="Website" link_to_resource="../../properties/elc-home-mar2022.jpeg" description="lengendary loreum texts"></ResourceCard>
+          <ResourceCard title="Doc Title2" resource_type="Doc" link_to_resource="../../properties/elc-home-mar2022.jpeg" description="Lorem ipsum dolor sit amet, in sed percipitur eloquentia Lorem ipsum dolor sit amet, in sed"></ResourceCard>
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={3}>
+        <Grid item md={6} xs={12}>
+          <ResourceCard title="Website Title1" resource_type="Website" link_to_resource="../../properties/elc-home-mar2022.jpeg" description="Lorem ipsum dolor sit amet, in sed percipitur eloquentia Lorem ipsum dolor sit amet, in sed"></ResourceCard>
+        </Grid>
+        <Grid item md={6} xs={12}>
+          <ResourceCard title="Website Title2" resource_type="Website" link_to_resource="../../properties/elc-home-mar2022.jpeg" description="Lorem ipsum dolor sit amet, in sed percipitur eloquentia Lorem ipsum dolor sit amet, in sed"></ResourceCard>
         </Grid>
       </Grid>
     </div>
