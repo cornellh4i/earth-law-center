25205a1e3b4fd71ddcc5b8e8c09d4e98bd3197f9
Jiayuan Gu <jiayuangu762943@gmail.com>
2022-04-23 22:38:14 -0400

    small changes

--- client/src/components/CategorySideBar/CategorySideBar.jsx
+++ client/src/components/CategorySideBar/CategorySideBar.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './CategorySideBar.css';
 
 /** Component for CategorySideBar Page 
--- client/src/pages/ResourceDisplay/ResourceDisplay.jsx
+++ client/src/pages/ResourceDisplay/ResourceDisplay.jsx
@@ -3,41 +3,46 @@ import './ResourceDisplay.css';
 import ResourceCard from '../../components/ResourceCard/ResourceCard';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
-
+import Header from "../../components/Header/Header"
+import CategorySideBar from "../../components/CategorySideBar/CategorySideBar"
 /** Component for Resource Display Page */
 const ResourceDisplay = () => {
   return (
-    <div>
+    <>
+      <Header />
+      <Grid container columns={16}>
+        <Typography pt={2} pb={2} align='left' variant='h5' component='div' sx={{ fontWeight: 'bold' }}>Resources</Typography>
+        <CategorySideBar categories={["ALL RESOURCES", "VIDEOS", "DOCUMENTS", "WEBSITES"]} />
+        <Grid container columns={8}>
+          <Grid container spacing={3}>
+            <Grid item md={6} xs={12}>
+              <ResourceCard title="Doc Title1" resource_type="Doc" link_to_resource="../../properties/elc-home-mar2022.jpeg" description="Lorem ipsum dolor sit amet, in sed percipitur eloquentia Lorem ipsum dolor sit amet, in sed" url='http://yahoo.com'></ResourceCard>
+            </Grid>
+            <Grid item md={6} xs={12}>
+              <ResourceCard title="Doc Title2" resource_type="Doc" link_to_resource="../../properties/elc-home-mar2022.jpeg" description="Lorem ipsum dolor sit amet, in sed percipitur eloquentia Lorem ipsum dolor sit amet, in sed" url='http://google.com'></ResourceCard>
+            </Grid>
+          </Grid>
 
-      <Typography pt={2} pb={2} align='left' variant='h5' component='div' sx={{ fontWeight: 'bold' }}>Resources</Typography>
+          <Grid container spacing={3}>
+            <Grid item md={6} xs={12}>
+              <ResourceCard title="Video Title1" resource_type="Video" link_to_resource="../../properties/elc-home-mar2022.jpeg" description="Lorem ipsum dolor sit amet, in sed percipitur eloquentia Lorem ipsum dolor sit amet, in sed" ></ResourceCard>
+            </Grid>
+            <Grid item md={6} xs={12}>
+              <ResourceCard title="Video Title2" resource_type="Video" link_to_resource="../../properties/elc-home-mar2022.jpeg" description="Lorem ipsum dolor sit amet, in sed percipitur eloquentia Lorem ipsum dolor sit amet, in sed"></ResourceCard>
+            </Grid>
+          </Grid>
 
-      <Grid container spacing={3}>
-        <Grid item md={6} xs={12}>
-          <ResourceCard title="Doc Title1" resource_type="Doc" link_to_resource="../../properties/elc-home-mar2022.jpeg" description="Lorem ipsum dolor sit amet, in sed percipitur eloquentia Lorem ipsum dolor sit amet, in sed" url='http://yahoo.com'></ResourceCard>
-        </Grid>
-        <Grid item md={6} xs={12}>
-          <ResourceCard title="Doc Title2" resource_type="Doc" link_to_resource="../../properties/elc-home-mar2022.jpeg" description="Lorem ipsum dolor sit amet, in sed percipitur eloquentia Lorem ipsum dolor sit amet, in sed" url='http://google.com'></ResourceCard>
+          <Grid container spacing={3}>
+            <Grid item md={6} xs={12}>
+              <ResourceCard title="Website Title1" resource_type="Website" link_to_resource="../../properties/elc-home-mar2022.jpeg" description="Lorem ipsum dolor sit amet, in sed percipitur eloquentia Lorem ipsum dolor sit amet, in sed"></ResourceCard>
+            </Grid>
+            <Grid item md={6} xs={12}>
+              <ResourceCard title="Website Title2" resource_type="Website" link_to_resource="../../properties/elc-home-mar2022.jpeg" description="Lorem ipsum dolor sit amet, in sed percipitur eloquentia Lorem ipsum dolor sit amet, in sed"></ResourceCard>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
-
-      <Grid container spacing={3}>
-        <Grid item md={6} xs={12}>
-          <ResourceCard title="Video Title1" resource_type="Video" link_to_resource="../../properties/elc-home-mar2022.jpeg" description="Lorem ipsum dolor sit amet, in sed percipitur eloquentia Lorem ipsum dolor sit amet, in sed" ></ResourceCard>
-        </Grid>
-        <Grid item md={6} xs={12}>
-          <ResourceCard title="Video Title2" resource_type="Video" link_to_resource="../../properties/elc-home-mar2022.jpeg" description="Lorem ipsum dolor sit amet, in sed percipitur eloquentia Lorem ipsum dolor sit amet, in sed"></ResourceCard>
-        </Grid>
-      </Grid>
-
-      <Grid container spacing={3}>
-        <Grid item md={6} xs={12}>
-          <ResourceCard title="Website Title1" resource_type="Website" link_to_resource="../../properties/elc-home-mar2022.jpeg" description="Lorem ipsum dolor sit amet, in sed percipitur eloquentia Lorem ipsum dolor sit amet, in sed"></ResourceCard>
-        </Grid>
-        <Grid item md={6} xs={12}>
-          <ResourceCard title="Website Title2" resource_type="Website" link_to_resource="../../properties/elc-home-mar2022.jpeg" description="Lorem ipsum dolor sit amet, in sed percipitur eloquentia Lorem ipsum dolor sit amet, in sed"></ResourceCard>
-        </Grid>
-      </Grid>
-    </div>
+    </>
   );
 };
 export default ResourceDisplay;
