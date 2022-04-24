bbc99b27e5d90192283c251c88a650925593a141
aoa9 <aoa9@cornell.edu>
2022-04-23 21:16:58 -0400

    set up progress bar

--- client/src/components/FieldSideBar/FieldSideBar.jsx
+++ client/src/components/FieldSideBar/FieldSideBar.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
 import Typography from '@mui/material/Typography';
 import { useRef } from 'react'
+import Box from '@mui/material/Box';
 import './FieldSideBar.css';
+import LinearProgress from '@mui/material/LinearProgress';
 
 /** Component for a Field Side Bar
  * @param {title} is the title header of the sidebar
@@ -10,6 +12,9 @@ import './FieldSideBar.css';
 
 const FieldSideBar = (props) => {
   const [clickedField, setClickedField] = useState('');
+  const [progress, setProgress] = useState(0);
+
+
   const fieldItem = props.field.map((field) =>
     <div>
       <div className='side-btn' onClick={() => handleClick(field)}>{field}</div>
@@ -24,6 +29,9 @@ const FieldSideBar = (props) => {
     <div className='fieldsidebar'>
       <Typography pt={5} pb={5} variant='h5' component='div' sx={{ fontWeight: 'bold' }}>{props.title}</Typography>
       {/* Space for progress bar */}
+      <Box sx={{ width: '100%' }}>
+      <LinearProgress variant="determinate" value={progress} />
+    </Box>
       <div className='bar-div'>{fieldItem}</div>
     </div>
   );
