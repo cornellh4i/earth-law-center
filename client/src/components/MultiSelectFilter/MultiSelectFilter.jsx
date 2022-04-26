a770b33eff05a037aa798a06f28a27cc53b7f250
Suhani Patel <sjp269@cornell.edu>
2022-04-25 22:31:36 -0400

    multiselect filter

--- client/src/components/MultiSelectFilter/MultiSelectFilter.jsx
+++ client/src/components/MultiSelectFilter/MultiSelectFilter.jsx
@@ -1,62 +1,73 @@
 import * as React from 'react';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import ListItemText from '@mui/material/ListItemText';
-import Select from '@mui/material/Select';
+import './MultiSelectFilter.css'
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
-};
+export default function MultiSelectFilter() {
 
-const names = [
-  'Local', 'Regional', 'National', 'International',
-];
-
-export default function MultipleSelectCheckmarks() {
-  const [personName, setName] = React.useState([]);
+  const [checked, setChecked] = React.useState(true);
 
   const handleChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-    setName(
-      // On autofill we get a stringified value.
-      typeof value === 'string' ? value.split(',') : value,
-    );
+    setChecked(event.target.checked);
   };
 
   return (
-    <div>
-      <FormControl sx={{ m: 1, width: 300 }}>
-        <InputLabel id="demo-multiple-checkbox-label">Tag</InputLabel>
-        <Select
-          labelId="demo-multiple-checkbox-label"
-          id="demo-multiple-checkbox"
-          multiple
-          value={personName}
-          onChange={handleChange}
-          input={<OutlinedInput label="Tag" />}
-          renderValue={(selected) => selected.join(', ')}
-          MenuProps={MenuProps}
-        >
-          {names.map((name) => (
-            <MenuItem key={name} value={name}>
-              <Checkbox checked={personName.indexOf(name) > -1} />
-              <ListItemText primary={name} />
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    </div>
+<Box className='box' component="span" sx={{ p: 2}}>
+<h1 className='text'>Filter By Jurisdiction</h1>
+<FormGroup>
+<FormControlLabel control={<Checkbox
+checked={checked}
+onChange={handleChange}
+inputProps={{ 'aria-label': 'controlled' }}
+/>} label={<h1 className = 'labels'>Local</h1>}/>
+</FormGroup>
+
+<FormGroup>
+<FormControlLabel control={<Checkbox
+checked={checked}
+onChange={handleChange}
+inputProps={{ 'aria-label': 'controlled' }}
+/>} label={<h1 className = 'labels'>Regional/State</h1>}/>
+</FormGroup>
+
+<FormGroup>
+<FormControlLabel control={<Checkbox
+checked={checked}
+onChange={handleChange}
+inputProps={{ 'aria-label': 'controlled' }}
+/>} label={<h1 className = 'labels'>National</h1>} />
+</FormGroup>
+
+<FormGroup>
+<FormControlLabel control={<Checkbox
+checked={checked}
+onChange={handleChange}
+inputProps={{ 'aria-label': 'controlled' }}
+/>} label={<h1 className = 'labels'>International</h1>} />
+</FormGroup>
+<br></br>
+
+<h1 className='text'>Filter By Type of Law</h1>
+<FormGroup>
+<FormControlLabel control={<Checkbox
+checked={checked}
+onChange={handleChange}
+inputProps={{ 'aria-label': 'controlled' }}
+/>} label={<h1 className = 'labels'>Ordinance</h1>} />
+</FormGroup>
+
+<FormGroup>
+<FormControlLabel className='labels' control={<Checkbox
+checked={checked}
+onChange={handleChange}
+inputProps={{ 'aria-label': 'controlled' }}
+/>} label={<h1 className = 'labels'>Resolution</h1>}/>
+</FormGroup>
+</Box>
   );
 }
+
+
