4af9098790ffa5e8fc145fa81412cad26b570462
Suhani Patel <sjp269@cornell.edu>
2022-04-27 21:09:40 -0400

    fixed checkbox grouping issue

--- client/src/components/MultiSelectFilter/MultiSelectFilter.jsx
+++ client/src/components/MultiSelectFilter/MultiSelectFilter.jsx
@@ -4,48 +4,64 @@ import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
 
 export default function MultiSelectFilter() {
 
-  const [checked, setChecked] = React.useState(true);
+  const [state, setState] = React.useState({
+    local: false,
+    regional: false,
+    national: false,
+    international: false,
+    ordinance: false,
+    resolution: false,
+
+  });
 
   const handleChange = (event) => {
-    setChecked(event.target.checked);
+    setState({
+      ...state,
+      [event.target.name]: event.target.checked,
+    });
   };
 
+  const { local, regional, national, international, ordinance, resolution } = state;
   return (
 <Box className='box' component="span" sx={{ p: 2}}>
 <h1 className='text'>Filter By Jurisdiction</h1>
+
 <FormGroup>
 <FormControlLabel control={<Checkbox
-checked={checked}
+checked={local}
 onChange={handleChange}
 inputProps={{ 'aria-label': 'controlled' }}
+name = 'local'
 />} label={<h1 className = 'labels'>Local</h1>}/>
 </FormGroup>
 
 <FormGroup>
 <FormControlLabel control={<Checkbox
-checked={checked}
+checked={regional}
 onChange={handleChange}
 inputProps={{ 'aria-label': 'controlled' }}
+name = 'regional'
 />} label={<h1 className = 'labels'>Regional/State</h1>}/>
 </FormGroup>
 
 <FormGroup>
 <FormControlLabel control={<Checkbox
-checked={checked}
+checked={national}
 onChange={handleChange}
 inputProps={{ 'aria-label': 'controlled' }}
+name = 'national'
 />} label={<h1 className = 'labels'>National</h1>} />
 </FormGroup>
 
 <FormGroup>
 <FormControlLabel control={<Checkbox
-checked={checked}
+checked={international}
 onChange={handleChange}
 inputProps={{ 'aria-label': 'controlled' }}
+name = 'international'
 />} label={<h1 className = 'labels'>International</h1>} />
 </FormGroup>
 <br></br>
@@ -53,21 +69,21 @@ inputProps={{ 'aria-label': 'controlled' }}
 <h1 className='text'>Filter By Type of Law</h1>
 <FormGroup>
 <FormControlLabel control={<Checkbox
-checked={checked}
+checked={ordinance}
 onChange={handleChange}
 inputProps={{ 'aria-label': 'controlled' }}
+name = 'ordinance'
 />} label={<h1 className = 'labels'>Ordinance</h1>} />
 </FormGroup>
 
 <FormGroup>
 <FormControlLabel className='labels' control={<Checkbox
-checked={checked}
+checked={resolution}
 onChange={handleChange}
 inputProps={{ 'aria-label': 'controlled' }}
+name = 'resolution'
 />} label={<h1 className = 'labels'>Resolution</h1>}/>
 </FormGroup>
 </Box>
   );
 }
-
-
