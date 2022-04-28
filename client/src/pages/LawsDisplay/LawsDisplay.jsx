fd8b041f2ec9f1e100fba3d7325608cb813946ae
Suhani Patel <sjp269@cornell.edu>
2022-04-28 01:41:07 -0400

    added in header and made category bar and filter same size- slight issues with styling

--- client/src/pages/LawsDisplay/LawsDisplay.jsx
+++ client/src/pages/LawsDisplay/LawsDisplay.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './LawsDisplay.css';
 import '../../components/CategorySideBar/CategorySideBar'; 
+import Header from '../../components/Header/Header';
 import CategorySideBar from '../../components/CategorySideBar/CategorySideBar';
 import TemplateCard from '../../components/TemplateCard/TemplateCard'; 
 import MultiSelectFilter from '../../components/MultiSelectFilter/MultiSelectFilter'
@@ -9,6 +10,7 @@ import MultiSelectFilter from '../../components/MultiSelectFilter/MultiSelectFil
 const LawsDisplay = () => {
   return (
     <div>
+      <Header title="Build Earth Laws" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." hasSearch={true} searchBarPlaceholder="Search Law Templates"></Header>
       <h1>LawsDisplay</h1>
       <CategorySideBar categories = {["ALL CATEGORIES", "RIGHTS OF NATURE", "RIGHTS OF FUTURE GENERATIONS", "HUMAN ENVIRONMENTAL RIGHTS", 
                                     "ECOCIDE", "LEGAL GUARDIANSHIP", "ECOCENTRIC CORPORATE GOVERNANCE", "ECOCENTRIC LAND MODELS"]}/>
