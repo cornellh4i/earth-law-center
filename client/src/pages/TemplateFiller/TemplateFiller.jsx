0a25b04f323b8a4a28872c658e94b495b6c41cc0
Jason Zheng <jasonz4200@gmail.com>
2022-05-04 20:48:12 -0400

    Add more filler to align with Figma

--- client/src/pages/TemplateFiller/TemplateFiller.jsx
+++ client/src/pages/TemplateFiller/TemplateFiller.jsx
@@ -31,7 +31,7 @@ const TemplateFiller = () => {
     'Enactment': {
       id: 3,
       questions: [
-        ['text input', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.']
+        ['text input', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.']
       ]
     }
   }
