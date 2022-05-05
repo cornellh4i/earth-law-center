7071274869c5231bc9841a0411a5fe58e5953e10
Joshua Feuerstein <jtf82@cornell.edu>
2022-05-04 22:08:32 -0400

    cleaned up class names in law and letter pages

--- client/src/pages/LettersDisplay/LettersDisplay.jsx
+++ client/src/pages/LettersDisplay/LettersDisplay.jsx
@@ -19,7 +19,7 @@ const LettersDisplay = () => {
           <MultiSelectFilter ref={select_filter}/>
         </div>
         <div className="content-box">
-          <h1 className="card-title">Letter Templates</h1>
+          <h1 className="card-heading-title">Letter Templates</h1>
           <p className="card-description">Description of category: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,</p>
           <div className="card-box">
             <TemplateCard letter={true} preview={testPic} className="law-card" title="Ecocentric Land" summary="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."/>
