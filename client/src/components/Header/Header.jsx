509b7c3afe089c3572413aa9e285e4674f86ca4b
Ananya Seelam <as2759@cornell.edu>
2022-05-06 11:05:38 -0400

    Fixing some issues with header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -33,7 +33,7 @@ const Header = (props) => {
           <Typography pt={5} pb={1} variant='h2' component='div' sx={{ fontWeight: 'bolder', fontFamily: 'Nunito', color: '#64926E'}}>
             {props.title}
           </Typography>
-          <Typography variant='body2' color='text.secondary' sx={{ fontSize: 18, marginTop: 4, marginRight: 20}} >
+          <Typography variant='body2' color='text.secondary' sx={{ fontSize: 18, fontFamily: 'Nunito', marginTop: 4, marginRight: 20}} >
             {props.description}
           </Typography>
           <div className="search-bar-container">
@@ -52,10 +52,10 @@ const Header = (props) => {
     <div className="banner">
       <NavBar/>
       <div className="content">
-        <Typography pt={5} pb={1} variant='h2' component='div' sx={{ fontWeight: 'bold' }}>
+        <Typography pt={5} pb={1} variant='h2' component='div' sx={{ fontWeight: 'bolder', fontFamily: 'Nunito', color: '#64926E'}}>
           {props.title}
         </Typography>
-        <Typography variant='body2' color='text.secondary' sx={{ fontSize: 18, marginTop: 6, marginRight: 46 }} >
+        <Typography variant='body2' color='text.secondary' sx={{ fontSize: 18, fontFamily: 'Nunito', marginTop: 6, marginRight: 46 }} >
           {props.description}
         </Typography>
       </div>
