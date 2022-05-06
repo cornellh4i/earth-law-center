850a273837e76db960d82a442ffc55ab8c2648e2
Ananya Seelam <as2759@cornell.edu>
2022-05-06 17:55:15 -0400

    Adding changes

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -6,44 +6,45 @@ import LandingCard from '../../components/LandingCard/LandingCard';
 import Header from '../../components/Header/Header'; 
 import './Landing.css';
 import { Link } from 'react-router-dom'
-import Text from '../../data/LandingText.js';
+import text from '../../data/LandingText.js';
 
 /** Component for Landing Page */
 const Landing = () => {
   return (
     <div>
-      <Header title="EarthLegislator" description={Text.HeaderDescription} hasSearch={false}/>
+      <Header title="EarthLegislator" description={text.HeaderDescription} hasSearch={false}/>
       <div className="content">
         {/* We have two Landing boxes for now */}
         <Grid container spacing={4} pt={5}>
           <Grid item xs={12} md={5.5}>
-              <LandingCard
-                title={"Law Template"}
-                text={Text.LawsDescription}>
-                link = {'/laws'}
-              </LandingCard>
+            <Link to='/laws' style = {{textDecoration: 'none'}}><LandingCard
+              title={"Law Template"}
+              text={text.LawsDescription}>
+            </LandingCard></Link>
           </Grid>
           <Grid item xs={12} md={1}></Grid>
           <Grid item xs={12} md={5.5}>
-            <LandingCard
+          <Link to='/letters' style = {{textDecoration: 'none'}}><LandingCard
               title={"Letter Template"}
-              text={Text.LettersDescription}>
-            </LandingCard>
+              text={text.LettersDescription}>
+            </LandingCard></Link>
           </Grid>
         </Grid>
 
-        {/* Three resource boxes I believe. We can probably useState to fix them later */}
-        <Typography pt={10} pb={2} align='center' variant='h5' component='div' sx={{ fontWeight: '900', fontSize: '36', fontFamily: 'Nunito', color: '#64926E' }}>Resources</Typography>
+        {/*Three resource boxes I believe. We can probably useState to fix them later*/}
+        <Typography pt={10} pb={2} align='center' variant='h5' component='div' sx={{ fontWeight: 'bold' }}>Resources</Typography>
 
         <Grid container spacing={4}>
           <Grid item xs={12} md={4}>
-            <ResourceBox></ResourceBox>
+            <ResourceBox boxNum = "1" title = "Video Title" description="Lorem">
+
+            </ResourceBox>
           </Grid>
           <Grid item xs={12} md={4}>
-            <ResourceBox></ResourceBox>
+            <ResourceBox boxNum ="2" title = "Video Title" description="Lorem"></ResourceBox>
           </Grid>
           <Grid item xs={12} md={4}>
-            <ResourceBox></ResourceBox>
+            <ResourceBox boxNum ="3" title = "Video Title" description="Lorem"></ResourceBox>
           </Grid>
         </Grid>
       </div>
