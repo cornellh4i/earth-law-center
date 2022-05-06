1e214d617da3240c45e4c144901f943d86150a60
Ananya Seelam <as2759@cornell.edu>
2022-05-06 11:50:43 -0400

    implementing the landing card styling and functionality

--- client/src/data/LandingText.js
+++ client/src/data/LandingText.js
@@ -1,3 +1,8 @@
 const HeaderDescription = "Earth Legislator is a web portal with ecocentric (or “Earth-centered”) legal models that legislators, activists, and others can easily customize and download—much like the popular websites “Rocket Lawyer” or “LegalZoom” but for the planet. It also features customizable letter templates, toolkits, and other resources, making it easier for anyone to help ecocentric legal movements flourish across the globe. These legal movements aim to address shortcomings of the current legal system, which legalizes the destruction of nature, so that we achieve a future in which humans and Nature flourish together."
 
-export default HeaderDescription
+const LawsDescription = "Customize and download model laws, resolutions, and other legal instruments that support a paradigm shift to better protect the planet."
+
+const LettersDescription = "Customize and download letter templates encouraging governments, businesses, schools, and other actors to support Earth-centered legal movements."
+
+
+export default {HeaderDescription, LawsDescription, LettersDescription}
--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -6,33 +6,38 @@ import LandingCard from '../../components/LandingCard/LandingCard';
 import Header from '../../components/Header/Header'; 
 import './Landing.css';
 import { Link } from 'react-router-dom'
-import HeaderDescription from '../../data/LandingText.js';
+import Text from '../../data/LandingText.js';
 
 /** Component for Landing Page */
 const Landing = () => {
   return (
     <div>
-      <Header title="EarthLegislator" description={HeaderDescription} hasSearch={false}/>
+      <Header title="EarthLegislator" description={Text.HeaderDescription} hasSearch={false}/>
       <div className="content">
         {/* We have two Landing boxes for now */}
         <Grid container spacing={4} pt={5}>
           <Grid item xs={12} md={5.5}>
-            <Link to='/laws' style = {{textDecoration: 'none'}}><LandingCard
-              title={"Law Template"}
-              text={"One sentence description of what is on this webpage"}>
-            </LandingCard></Link>
+            {/* <Link to='/laws' style = {{textDecoration: 'none'}}> */}
+              <LandingCard
+                title={"Law Template"}
+                text={Text.LawsDescription}>
+                link = {'laws'}
+              </LandingCard>
+            {/* </Link> */}
           </Grid>
           <Grid item xs={12} md={1}></Grid>
           <Grid item xs={12} md={5.5}>
-          <Link to='/letters' style = {{textDecoration: 'none'}}><LandingCard
+          <Link to='/letters' style = {{textDecoration: 'none'}}>
+            <LandingCard
               title={"Letter Template"}
-              text={"One sentence description of what is on this webpage"}>
-            </LandingCard></Link>
+              text={Text.LettersDescription}>
+            </LandingCard>
+          </Link>
           </Grid>
         </Grid>
 
         {/*Three resource boxes I believe. We can probably useState to fix them later*/}
-        <Typography pt={10} pb={2} align='center' variant='h5' component='div' sx={{ fontWeight: 'bold' }}>Resources</Typography>
+        <Typography pt={10} pb={2} align='center' variant='h5' component='div' sx={{ fontWeight: 'bolder',fontFamily: 'Nunito' }}>Resources</Typography>
 
         <Grid container spacing={4}>
           <Grid item xs={12} md={4}>
