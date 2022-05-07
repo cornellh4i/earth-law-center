36bc2eb67a7e175189efe036c8577205aa5397db
Ananya Seelam <as2759@cornell.edu>
2022-05-07 12:02:07 -0400

    editing resourcebox

--- client/src/components/ResourceBox/ResourceBox.jsx
+++ client/src/components/ResourceBox/ResourceBox.jsx
@@ -7,7 +7,7 @@ import { CardActionArea } from '@mui/material';
 /** Component for a Resource card */
 const ResourceBox = (boxNum, title, description) => {
   return (
-    <Card className={boxNum == "1" ? "resource1" : boxNum == "2" ? "resource2" : "resource3"}>
+    <Card className={boxNum === "1" ? 'resource1' : boxNum === "2" ? 'resource2' : 'resource3'}>
       <CardActionArea>
         <CardContent className='box-content'></CardContent>
       </CardActionArea>
