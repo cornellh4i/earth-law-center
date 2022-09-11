4102ffd715abf0621e9138cb88c9df3f1f4ea73b
Ananya Seelam <as2759@cornell.edu>
2022-09-11 17:33:38 -0400

    Adding scopes for google sheets and changed some file names

--- server/src/routes.js
+++ server/src/routes.js
@@ -7,7 +7,7 @@ module.exports = () => {
   // Imported functions
   const functions = require('../gdocs-api/functions.js');
   const authsamp = require('../gdocs-api/web_index.js')
-  const scopes = ['https://www.googleapis.com/auth/documents', 'https://www.googleapis.com/auth/drive'];
+  const scopes = ['https://www.googleapis.com/auth/documents', 'https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/sheets'];
 
   /**** Below are Example Routes from the starter code *****/
   router.get('/hello', async (res) => {
