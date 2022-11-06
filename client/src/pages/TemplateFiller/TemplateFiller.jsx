cb869c07437dcf5ea21c521e2253c6339357bd79
Ananya Seelam <as2759@cornell.edu>
2022-11-06 17:18:34 -0500

    making docID not hardcoded in batch replace fetch

--- client/src/pages/TemplateFiller/TemplateFiller.jsx
+++ client/src/pages/TemplateFiller/TemplateFiller.jsx
@@ -114,7 +114,7 @@ const TemplateFiller = () => {
     // }
 
     // need to include docID, currently hard coded docID for sample template in URL
-    fetch(`/api/batchReplaceAllTexts/13YIr9SZ2Vgkh3R56AGP4L3cUIXZEmVqygHJ3IOj_BeQ`, {
+    fetch(`/api/batchReplaceAllTexts/${data.docID}`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(batchReplaceData)
