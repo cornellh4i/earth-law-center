350198de47557ddd86f4c2a4f9e501e575b5c935
Ananya Seelam <as2759@cornell.edu>
2022-11-13 19:20:48 -0500

    Making small change in /preAuthenticate endpoint so we can remove the preAuthenticate function

--- server/src/routes.js
+++ server/src/routes.js
@@ -113,7 +113,7 @@ module.exports = () => {
   // Endpoint for preAuthenticate
   router.get('/preAuthenticate', async (req, res) => {
     try {
-      functions.readAuthFile().then((res) => res.json({ msg: `token: ${response}` }))
+      functions.readAuthFile().then((response) => res.json({ msg: `token: ${response}` }))
     }
     catch (err) {
       res.json({ error: err.message || err.toString() })
