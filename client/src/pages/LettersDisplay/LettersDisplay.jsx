56a5ca0dd9532d46114f0a1ec3f712158e3f90ba
Jason Zheng <jasonz4200@gmail.com>
2022-12-04 18:17:07 -0500

    Fix padding issues for Laws, Letters, and ResourceDisplay

--- client/src/pages/LettersDisplay/LettersDisplay.jsx
+++ client/src/pages/LettersDisplay/LettersDisplay.jsx
@@ -34,17 +34,17 @@ const LettersDisplay = () => {
   return (
     <div>
       <Header title="Build Earth Letters" description="Customize and download model laws, resolutions, and other legal instruments that support a paradigm shift to better protect the planet. " hasSearch={true} searchBarPlaceholder="Search Letter Templates" handleSearch={searchData}></Header>
-      <div className="letters-body-box">
+      <div className="content">
         {/* <div className="side-box">
         <MultiSelectFilter setCheckedParent={setChecked} />
         </div> */}
-        <div className="letter-content-box">
+        {/* <div className="letter-content-box"> */}
           <h1 className="card-heading-title">Letter Templates</h1>
           <p className="card-description">Let us help you send a lawmaker or other decision-maker an informed letter about an Earth law movement you care about–the Rights of Nature, human environmental rights, rights of future generations, and others. Our letters are regularly updated with the most relevant information to ensure they are relevant and powerful. Just fill in a bit of information and download your letter now! These work for any level of government.</p>
           <div className="letter-template-box">
             <TemplateList data={data} checked={checked} />
           </div>
-        </div>
+        {/* </div> */}
       </div>
       <Footer text="Powered by Earth Law Center" />
     </div>
